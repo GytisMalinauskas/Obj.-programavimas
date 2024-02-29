a7f011cee71621757cc33324388de8cb5a191f99
GytisMalinauskas <159523970+GytisMalinauskas@users.noreply.github.com>
2024-02-29 17:25:25 +0200

    Update v.pradine.cpp

--- v.pradine.cpp
+++ v.pradine.cpp
@@ -5,7 +5,7 @@ struct studentas
 {
   string name;
   string surname;
-  double HWresults[2];
+  double nd[2];
   double Eresults;
   double average;
   double grade;
@@ -20,17 +20,17 @@ int main()
   a[1].name = "Marius";
   a[0].surname = "Malinauskas";
   a[1].surname = "Pavardenis";
-  a[0].HWresults[0]=10;
-  a[0].HWresults[1]=15;
-  a[1].HWresults[0]=57.5;
-  a[1].HWresults[1]=68.25;
+  a[0].nd[0]=10;
+  a[0].nd[1]=15;
+  a[1].nd[0]=57.5;
+  a[1].nd[1]=68.25;
   a[0].Eresults=100;
   a[1].Eresults=75.25;
   for(int i=0; i<StudentuSkaicius; i++)
   {
   for(int j=0; j<n; j++)
   {
-  sum+=a[i].HWresults[j];
+  sum+=a[i].nd[j];
   }
   a[i].average=sum/n;
   }
