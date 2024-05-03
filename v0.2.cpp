4b4c6101017720600bd4595ccf72239e4f4bda9d
GytisMalinauskas <159523970+GytisMalinauskas@users.noreply.github.com>
2024-05-03 08:51:19 +0300

    Update v0.2.cpp

--- v0.2.cpp
+++ v0.2.cpp
@@ -1,358 +1,354 @@
+#include <cstdio>
+#include <locale>
 #include <time.h>
-#include <algorithm>
+#include <limits>
+#include <iostream>
 #include <fstream>
 #include <iomanip>
-#include <iostream>
-#include <locale>
-#include <random>
-#include <sstream>
-#include <string>
 #include <vector>
-
+#include <sstream>
+#include <algorithm>
+#include <ctime>
+#include <cstdlib>
+#include <string>
+#include <cstring> 
+#include <cerrno>
+#include <random> 
 using namespace std;
-
-struct Studentas {
-    string vardas;
-    string pavarde;
-    int n;
-    vector<int> nd;
-    int egz;
-    double vid = 0;
-    double mediana;
+struct studentas
+{
+  string name;
+  string surname;
+  double Eresults;
+  double average;
+  double grade;
+  vector <double> nd;
+  double median;
+  int ndCount;
+  double sum;
 };
-vector<string> vardai;
-vector<string> pavardes;
+vector<string> names;
+vector<string> surnames;
 void initializeNamesAndSurnames() {
-    vardai.push_back("John");
-    vardai.push_back("Emma");
-    vardai.push_back("Michael");
-    vardai.push_back("Sophia");
-    vardai.push_back("William");
-    vardai.push_back("Olivia");
-    vardai.push_back("James");
-    vardai.push_back("Ava");
-    vardai.push_back("Alexander");
-    vardai.push_back("Isabella");
+    names.push_back("John");
+    names.push_back("Emma");
+    names.push_back("Michael");
+    names.push_back("Sophia");
+    names.push_back("William");
+    names.push_back("Olivia");
+    names.push_back("James");
+    names.push_back("Ava");
+    names.push_back("Alexander");
+    names.push_back("Isabella");
 
-    pavardes.push_back("Smith");
-    pavardes.push_back("Johnson");
-    pavardes.push_back("Williams");
-    pavardes.push_back("Jones");
-    pavardes.push_back("Brown");
-    pavardes.push_back("Davis");
-    pavardes.push_back("Miller");
-    pavardes.push_back("Wilson");
-    pavardes.push_back("Moore");
-    pavardes.push_back("Taylor");
+    surnames.push_back("Smith");
+    surnames.push_back("Johnson");
+    surnames.push_back("Williams");
+    surnames.push_back("Jones");
+    surnames.push_back("Brown");
+    surnames.push_back("Davis");
+    surnames.push_back("Miller");
+    surnames.push_back("Wilson");
+    surnames.push_back("Moore");
+    surnames.push_back("Taylor");
+}
+string generateRandomName(vector <string> names) {
+    string name2 = names[rand() % names.size()];
+    return name2;
+}
+string generateRandomSurname(vector <string> surnames) {
+    string surname2 = surnames[rand() % surnames.size()];
+    return surname2;
 }
 
-void duomSkait(vector<Studentas>& studentai);
-void ivestis(vector<Studentas>& studentai);
-void atvaizd(vector<Studentas>& studentai, int m, int rikiavimas);
-string stringIvestis();
-int intIvestis(int sRange, int eRange);
-int randGen(int sRange, int eRange);
-double rastiVid(Studentas studentasTemp);
-double rastiMed(Studentas studentasTemp);
+void Sorting(vector<studentas>& s, string choice, int rikiavimas);
 
-double rastiVid(vector<Studentas>& studentai, int m);
-double rastiMed(vector<Studentas>& studentai, int m);
-
-int main() {
-    srand(time(0));
-    cout << "Pasirinkite įvedimo būdą: " << endl
-         << "(1) Rankinis įvedimo būdas" << endl
-         << "(2) Skaitymas iš failo" << endl;
-
-    int cInput = intIvestis(1, 2);
-    vector<Studentas> studentai;
-    
-    if (cInput == 1) {
-        ivestis(studentai);
-    } else if (cInput == 2) {
-        duomSkait(studentai);
+int main() 
+{
+  srand(time(0));
+  int MinP=1, MaxP=10;
+  int m=0,n,b;
+  vector <studentas> s;
+  while (true)
+  {
+  cout<<"Enter number"<<endl; 
+  cout<<"1 - manual input"<<endl;
+  cout<<"2 - generate grades"<<endl;
+  cout<<"3 - generate names and grades"<<endl;
+  cout<<"4 - reading from file"<<endl;
+  cout<<"5 - quit / show results"<<endl;
+  cout<<"Number: ";
+  int skaicius;
+  cin>>skaicius;
+  if(skaicius==5)
+  {break;}
+  int number2;
+  if(skaicius==3)
+  {
+    cout<<"Enter number"<<endl;
+    cout<<"1 - generate name and surname by pc"<<endl;
+    cout<<"2 - manually input names and surnames for generating students"<<endl;
+    cout<<"Number: ";
+    cin>>number2;
+    if(number2==1)
+    {
+      initializeNamesAndSurnames();
     }
-    return 0;
-}
-
-void duomSkait(vector<Studentas>& studentai) {
+    if(number2==2)
+    {
+      while(true)
+      {
+        string vardas, pavarde;
+        cout<<"Enter name or type 'quit' to quit: ";
+        cin>>vardas;
+        if(vardas=="quit")
+        {break;}
+        cout<<"Enter surname: ";
+        cin>>pavarde;
+        names.push_back(vardas);
+        surnames.push_back(pavarde);
+      }
+    }
+  }
+  if(skaicius!=4){
+  cout<<"Enter number"<<endl;
+  cout<<"1 - n and m is given"<<endl;
+  cout<<"2 - n and m is not given"<<endl;
+  cout<<"Number: ";
+  int number1; 
+  cin>>number1;
+    if(number1==1)
+    {
+      cout<<"Enter the number of students: "; cin>>b;
+      cout<<"Enter the number of homework: "; cin>>n;
+      m+=b;
+      if(m-b==0)
+      {b=0;}
+      s.resize(m);
+      for(int i=b; i<m; i++)
+      {
+        if(skaicius==1||skaicius==2)
+        {
+          cout<<"Enter name: "; cin>>s[i].name; 
+          cout<<"Enter Surname: "; cin>>s[i].surname;
+        }
+        else if(skaicius==3)
+        {
+            s[i].name=generateRandomName(names);
+            s[i].surname=generateRandomSurname(surnames); 
+        }
+          for(int j=0; j<n; j++)
+          {
+            double result=0;
+            if(skaicius==1)
+            {
+              cout<<"Enter homework result "<<j+1<<": ";
+              cin>>result;
+            }
+            if(skaicius==2||skaicius==3)
+            {
+              result=MinP+rand()%(MaxP-MinP+1);
+            }
+            s[i].nd.push_back(result);
+            s[i].sum+=result;
+          }
+            if(skaicius==1)
+            {
+              cout<<"Enter exam results: ";
+              cin>>s[i].Eresults;
+            }
+            if(skaicius==2||skaicius==3)
+            {
+              s[i].Eresults=MinP+rand()%(MaxP-MinP+1);
+            }
+      sort(s[i].nd.begin(),s[i].nd.end());
+      s[i].median=0;
+      if(n%2!=0)
+      {s[i].median=s[i].nd[n/2];}
+      if(n%2==0)
+      {s[i].median=(s[i].nd[n/2]+s[i].nd[n/2+1])/2.0;}
+      s[i].average=s[i].sum/n;
+      s[i].grade=s[i].average*0.4+0.6*s[i].Eresults;
+      }
+    }
+    else if(number1 == 2)
+    {
+      while(true)
+      {
+        if(skaicius==1||skaicius==2)
+        {
+          string vardas;
+          cout<<"Enter name or type 'quit' to quit: "; 
+          cin>>vardas;
+          if (vardas=="quit")
+          {break;}
+          s[m].name=vardas;
+          cout<<"Enter surname: ";
+          cin>>s[m].surname;
+        } 
+        if(skaicius==3)
+        {
+          string gq;
+          cout<<"Enter g (to generate 1 student) or q (to quit): "; 
+          cin>>gq;
+          if(gq=="q")
+          {break;}
+          else if(gq=="g")
+          {
+            s[m].name=generateRandomName(names);
+            s[m].surname=generateRandomSurname(surnames);
+          }
+        }
+        s[m].ndCount=0;
+        double result;  
+        if(skaicius==1)
+        {
+          while(true)
+          { 
+          cout<<"Enter homework results or type '100' to quit: "; 
+          cin>>result; 
+          if(result == 100) 
+          {break;}
+          s[m].ndCount++;
+          s[m].nd.push_back(result);
+          s[m].sum+=result;
+          }
+        }
+        if(skaicius==2||skaicius==3)
+        {
+          while(true)
+          {
+            string gq2;
+            cout<<"Enter g (to generate "<<s[m].ndCount+1<<" homework result) or q (to quit): "; 
+            cin>>gq2;
+            if(gq2=="q")
+            {break;}
+            else if(gq2=="g")
+            {
+              result=MinP+rand()%(MaxP-MinP+1);
+              s[m].ndCount++;
+              s[m].nd.push_back(result);
+              s[m].sum+=result;
+            }
+          } 
+        }
+        if(skaicius==1)
+        {
+          cout<<"Enter exam result: ";
+          cin>>s[m].Eresults;
+        }
+        if(skaicius==2||skaicius==3)
+        {
+          s[m].Eresults=MinP+rand()%(MaxP-MinP+1);
+        }
+        sort(s[m].nd.begin(),s[m].nd.end());
+        s[m].median=0;
+        if (s[m].ndCount%2!=0)
+        {s[m].median=s[m].nd[s[m].ndCount/2];}
+        if (s[m].ndCount%2==0)
+        {s[m].median=(s[m].nd[s[m].ndCount/2]+s[m].nd[s[m].ndCount/2+1])/2.0;}
+        s[m].average=s[m].sum/s[m].ndCount;
+        s[m].Eresults=MinP+rand()%(MaxP-MinP+1);
+        s[m].grade=s[m].average*0.4+0.6*s[m].Eresults;
+        m++;
+        s.resize(m+1);
+      }   
+    }
+      names.clear();
+      surnames.clear();
+      cout<<endl;
+  }
+  if(skaicius==4)
+  {
     system("ls *.txt");
     cout << "Pasirinkite duomenų failą: ";
     string duomPav;
     cin >> duomPav;
 
     ifstream in(duomPav);
-    if (!in) {
-        cout << "Failas nerastas." << endl;
-        return;
+    try
+    {    
+        if (!in) 
+        {
+        throw runtime_error("Failas nerastas.");
+        }
     }
+    catch(const runtime_error& e)
+    {cout<<e.what()<<endl;}
 
     string line;
     getline(in, line);
 
     while (getline(in, line)) {
         std::istringstream iss(line);
-        Studentas s;
-        iss >> s.vardas >> s.pavarde;
-
+        studentas st;
+        iss >> st.name >> st.surname;
+        st.sum=0;
         int pazymys;
         while (iss >> pazymys) {
-            s.nd.push_back(pazymys);
-        }
-
-        s.egz = s.nd.back();
-        s.nd.pop_back();
-        s.n = s.nd.size();
-        s.vid = rastiVid(s);
-        s.mediana = rastiMed(s);
-        studentai.push_back(s);
+            st.nd.push_back(pazymys);
+            st.sum+=pazymys;        }
+        
+        st.Eresults = st.nd.back();
+        st.nd.pop_back();
+        st.ndCount = st.nd.size();
+        st.average = st.sum/st.ndCount;
+        if (st.ndCount%2!=0)
+        {st.median=st.nd[st.ndCount/2];}
+        if (st.ndCount%2==0)
+        {st.median=(st.nd[st.ndCount/2]+st.nd[st.ndCount/2+1])/2.0;}
+        st.grade=st.average*0.4+0.6*st.Eresults;
+        s.push_back(st);
+      }
+        
+  }
+  }
+    string choice;
+    cout<<"Choose either average or median: "; cin>>choice;
+    cout << "Choose how to sort: " << endl
+         << "1 - By name" << endl
+         << "2 - By surname" << endl
+         << "3 - By grade" << endl;
+    int sortmeth;
+    cin>>sortmeth;
+    Sorting(s,choice,sortmeth);
+    if (choice=="median")
+    {
+      cout<<"Name       Surname      Grade(Med.)"<<endl;
+      for(const auto& student : s)
+      {cout<<fixed<<setw(10)<<left<<student.name<<" "<<setw(12)<<left<<student.surname<<" "<<setprecision(2)<<setw(13)<<student.median<<endl;}
     }
-
-    cout << "Pasirinkite rikiavimo būdą: " << endl
-         << "(1) Pagal studento vardą" << endl
-         << "(2) Pagal studento pavarde" << endl
-         << "(3) Pagal studento galutinį pažymį" << endl;
-
-    atvaizd(studentai, studentai.size(), intIvestis(1, 3));
-}
-
-void ivestis(vector<Studentas>& studentai) {
-    bool loop = true;
-    unsigned int m = 0;
-    unsigned int n = 0;
-    while (loop) {
-        cout << "Pasirinkite įvedimo būdą:" << endl
-             << "(1) Duomenų įvedimas ranka" << endl
-             << "(2) Generuoti pažymius" << endl
-             << "(3) Generuoti vardą, pavardę ir pažymius" << endl
-             << "(4) Baigti darbą" << endl;
-
-        int cInput = intIvestis(0, 10);
-
-        if (cInput == 1) {
-            n = 0;
-
-            Studentas studentasTemp;
-
-            bool ndLoop = true;
-
-            cout << "Įveskite studento vardą: ";
-            studentasTemp.vardas = stringIvestis();
-            cout << "Įveskite studento pavardę: ";
-            studentasTemp.pavarde = stringIvestis();
-            cout << "Įveskite studento namų darbų pažymius (0 - pereiti prie egzamino pažymio įvesties): ";
-            while (ndLoop) {
-                int temp = intIvestis(0, 10);
-                if (temp != 0) {
-                    studentasTemp.nd.push_back(temp);
-                    n++;
-                } else {
-                    cout << "Įveskite studento egzamino pažymį: ";
-                    studentasTemp.egz = intIvestis(0, 10);
-                    studentasTemp.n = n + 1;
-                    studentasTemp.vid = rastiVid(studentasTemp);
-                    studentasTemp.mediana = rastiMed(studentasTemp);
-
-                    ndLoop = false;
-                }
-            }
-
-            studentai.emplace_back(studentasTemp);
-
-            m++;
-        } else if (cInput == 2) {
-            n = 0;
-
-            Studentas studentasTemp;
-
-            cout << "Įveskite studento vardą: ";
-            studentai[m].vardas = stringIvestis();
-            cout << "Įveskite studento pavardę: ";
-            studentai[m].pavarde = stringIvestis();
-
-            n = randGen(1, 10);
-            studentasTemp.n = n;
-            for (int i = 0; i < n; i++) {
-                studentasTemp.nd.push_back(randGen(1, 10));
-            }
-
-            studentasTemp.egz = randGen(1, 10);
-            studentasTemp.n = n;
-            studentasTemp.vid = rastiVid(studentasTemp);
-            studentasTemp.mediana = rastiMed(studentasTemp);
-
-            studentai.emplace_back(studentasTemp);
-
-            m++;
-        } else if (cInput == 3) {
-            n = 0;
-
-            Studentas studentasTemp;
-
-            studentasTemp.vardas = vardai[randGen(0, 6)];
-            studentasTemp.pavarde = pavardes[randGen(0, 6)];
-
-            n = randGen(1, 10);
-            studentasTemp.n = n;
-            for (int i = 0; i < n; i++) {
-                studentasTemp.nd.push_back(randGen(1, 10));
-            }
-
-            studentasTemp.egz = randGen(1, 10);
-            studentasTemp.n = n;
-            studentasTemp.vid = rastiVid(studentasTemp);
-            studentasTemp.mediana = rastiMed(studentasTemp);
-
-            m++;
-        } else if (cInput == 4) {
-            cout << "Pasirinkite rikiavimo būdą: " << endl
-                 << "(1) Pagal studento vardą" << endl
-                 << "(2) Pagal studento pavarde" << endl
-                 << "(3) Pagal studento galutinį pažymį" << endl;
-
-            atvaizd(studentai, m, intIvestis(1, 3));
-        }
+    else if (choice=="average")
+    {
+      cout<<"Name       Surname      Grade(Vid.)"<<endl;
+      for(const auto& student : s)
+      {cout<<fixed<<setw(10)<<left<<student.name<<" "<<setw(12)<<left<<student.surname<<" "<<setprecision(2)<<setw(13)<<student.grade<<endl;}
     }
 }
-
-string stringIvestis() {
-    string cInput;
-    bool loop = false;
-
-    do {
-        loop = false;
-        cin >> cInput;
-        for (char a : cInput) {
-            if (isblank(a) || isdigit(a) || cInput.length() > 16) {
-                loop = true;
-            }
-        }
-        if (loop) {
-            cout << "Klaida. Bandykite dar kartą: ";
-        }
-    } while (loop);
-
-    return cInput;
+   
+bool rikiavimasVardu(studentas& s1, studentas& s2) {
+    return s1.name < s2.name;
 }
 
-int intIvestis(int sRange, int eRange) {
-    int cInput;
-    bool loop = false;
-
-    do {
-        loop = false;
-        cin >> cInput;
-        if (cInput < sRange || cInput > eRange || !cin) {
-            cin.clear();
-            cin.ignore(numeric_limits<streamsize>::max(), '\n');
-            loop = true;
-        }
-        for (char a : to_string(cInput)) {
-            if (isblank(a)) {
-                cin.clear();
-                cin.ignore(numeric_limits<streamsize>::max(), '\n');
-                loop = true;
-            }
-        }
-        if (loop) {
-            cout << "Klaida. Bandykite dar kartą: ";
-        }
-    } while (loop);
-
-    return cInput;
+bool rikiavimasPavarde(studentas& s1, studentas& s2) {
+    return s1.surname < s2.surname;
 }
 
-int randGen(int sRange, int eRange) {
-    int r = rand() % eRange + sRange;
-
-    return r;
+bool rikiavimasVid(studentas& s1, studentas& s2) {
+    return s1.grade > s2.grade;
 }
-
-double rastiVid(Studentas studentasTemp) {
-    for (int i = 0; i < studentasTemp.n; i++) {
-        studentasTemp.vid += studentasTemp.nd[i];
-    }
-    studentasTemp.vid /= studentasTemp.n;
-
-    return studentasTemp.vid;
+bool rikiavimasMed(studentas& s1, studentas& s2) {
+    return s1.median > s2.median;
 }
-
-double rastiVid(vector<Studentas>& studentai, int m) {
-    for (int i = 0; i < studentai[m].n; i++) {
-        studentai[m].vid += studentai[m].nd[i];
-    }
-    studentai[m].vid /= studentai[m].n;
-
-    return studentai[m].vid;
-}
-
-double rastiMed(Studentas studentasTemp) {
-    for (int i = 0; i < studentasTemp.n; i++) {
-        for (int j = i + 1; j < studentasTemp.n; j++) {
-            if (studentasTemp.nd[i] < studentasTemp.nd[j]) {
-                int temp = studentasTemp.nd[i];
-                studentasTemp.nd[i] = studentasTemp.nd[j];
-                studentasTemp.nd[j] = temp;
-            }
-        }
-    }
-
-    if (studentasTemp.n % 2 == 0) {
-        studentasTemp.mediana = (studentasTemp.nd[studentasTemp.n / 2] + studentasTemp.nd[studentasTemp.n / 2 + 1]) / 2;
-    } else {
-        studentasTemp.mediana = studentasTemp.nd[studentasTemp.n / 2 + 1];
-    }
-
-    return studentasTemp.mediana;
-}
-
-double rastiMed(vector<Studentas>& studentai, int m) {
-    for (int i = 0; i < studentai[m].n; i++) {
-        for (int j = i + 1; j < studentai[m].n; j++) {
-            if (studentai[m].nd[i] < studentai[m].nd[j]) {
-                int temp = studentai[m].nd[i];
-                studentai[m].nd[i] = studentai[m].nd[j];
-                studentai[m].nd[j] = temp;
-            }
-        }
-    }
-
-    if (studentai[m].n % 2 == 0) {
-        studentai[m].mediana = (studentai[m].nd[studentai[m].n / 2] + studentai[m].nd[studentai[m].n / 2 + 1]) / 2;
-    } else {
-        studentai[m].mediana = studentai[m].nd[studentai[m].n / 2 + 1];
-    }
-
-    return studentai[m].mediana;
-}
-
-bool rikiavimasVardu(Studentas& s1, Studentas& s2) {
-    return s1.vardas < s2.vardas;
-}
-
-bool rikiavimasPavarde(Studentas& s1, Studentas& s2) {
-    return s1.pavarde < s2.pavarde;
-}
-
-bool rikiavimasPaz(Studentas& s1, Studentas& s2) {
-    return (s1.vid * 0.4 + s1.egz * 0.6) > (s2.vid * 0.4 + s2.egz * 0.6);
-}
-
-void atvaizd(vector<Studentas>& studentai, int m, int rikiavimas) {
+void Sorting(vector<studentas>& s, string choice, int rikiavimas) {
     if (rikiavimas == 1) {
-        sort(studentai.begin(), studentai.end(), rikiavimasVardu);
+        sort(s.begin(), s.end(), rikiavimasVardu);
     } else if (rikiavimas == 2) {
-        sort(studentai.begin(), studentai.end(), rikiavimasPavarde);
+        sort(s.begin(), s.end(), rikiavimasPavarde);
     } else if (rikiavimas == 3) {
-        sort(studentai.begin(), studentai.end(), rikiavimasPaz);
-    }
-
-    cout << left << setw(16) << "Vardas" << left << setw(16) << "Pavardė" << left << setw(16) << "Galutinis (Vid.)" << endl;
-    cout << "---------------------------------------------------------------------------" << m << endl;
-    for (int i = 0; i < m; i++) {
-        cout << left << setw(15) << studentai[i].vardas
-             << left << setw(16) << studentai[i].pavarde
-             << setprecision(2) << fixed << left << setw(16) << studentai[i].vid * 0.4 + double(studentai[i].egz) * 0.6
-             << setprecision(2) << fixed << left << setw(16) << studentai[i].mediana * 0.4 + double(studentai[i].egz) * 0.6 << endl;
+      if(choice=="average")
+        sort(s.begin(), s.end(), rikiavimasVid);
+      if(choice=="median")
+        sort(s.begin(), s.end(), rikiavimasMed);
     }
 }
