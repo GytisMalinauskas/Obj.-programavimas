e70ec3e72f4291ee52d70490d272d8c6f2b12481
GytisMalinauskas <159523970+GytisMalinauskas@users.noreply.github.com>
2024-05-09 21:41:37 +0300

    Add files via upload

---
+++ error.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <iostream>
+#include <cerrno>
+#include <limits>
+using namespace std;
+int intIvestis(int sRange, int eRange);
+int intIvestis2();
+char SimbolioTikrinimas();
+char SimbolioTikrinimas2();
+char SimbolioTikrinimas3();
