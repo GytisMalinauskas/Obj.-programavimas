98bda4eb915d7673c5931b83625931b711069e9d
GytisMalinauskas <159523970+GytisMalinauskas@users.noreply.github.com>
2024-05-16 18:53:34 +0300

    Delete deque directory

--- deque/fileGenerator.h
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <fstream>
-#include <cerrno>
-#include <stdexcept>
-#include <iomanip>
-
-using namespace std;
-
-void GenerateFile (int kiekis, int nd_kiekis, string &fileName);
