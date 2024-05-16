3e1ca6bf435c8705c6e2dc4853b45f2d35a2df18
GytisMalinauskas <159523970+GytisMalinauskas@users.noreply.github.com>
2024-05-16 17:05:39 +0300

    Add files via upload
    Iki galo sutvarkytos programos

---
+++ list/studentas.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <list>
+#include <string>
+#include <iostream>
+using namespace std;
+struct studentas
+{
+  string name;
+  string surname;
+  double Eresults;
+  double average;
+  double grade;
+  list <double> nd;
+  double median;
+  int ndCount;
+  double sum;
+};
---
+++ vector/main.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <iostream>
+#include "studentas.h"
+#include <cerrno>
+#include "error.h"
+#include "function.h"
+#include "fileGenerator.h"
